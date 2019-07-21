facfb5d4f8bb537bd6aebd16aa17903ef32c3820
candeiasalexandre <candeiasalexandre@gmail.com>
2019-07-22 00:54:51 +0100

    working without conda

--- face_landmarks.py
+++ face_landmarks.py
@@ -7,7 +7,6 @@ class MenpoDlibLandmarkDetector():
         self.face_landmark_detector = dlib.shape_predictor(model_path)
 
     def detect(self, image, bb):
-        
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         dlib_rectangle = dlib.rectangle(int(bb[0]), int(bb[1]), int(bb[2]), int(bb[3]) )
         landmarks = self.face_landmark_detector(gray, dlib_rectangle)
